23ce392e4c76f8fff41325b49bafd4a0af96f6b9
Christian <chrisvergara7@outlook.com>
2024-10-30 22:29:36 -0600

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 // import { useState } from 'react'
 
 import { Principal } from './pages/Principal'
-import {Route, Routes, BrowserRouter, Navigate} from 'react-router-dom'
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
 
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <Navigate to="/weather"/> }></Route>
-          <Route path="/weather" element={<Principal />}></Route>
+          <Route path="/" element={<Principal />}></Route>
         </Routes>
     </BrowserRouter>
   )
